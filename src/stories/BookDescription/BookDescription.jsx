889e113306a2dca8d932395993d19dbb482d27a3
Diego H <d13g01hl@gmail.com>
2023-05-23 17:17:21 -0500

    Minor changes

--- src/stories/BookDescription/BookDescription.jsx
+++ src/stories/BookDescription/BookDescription.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { Typography } from '@mui/material';
 
 
 export const BookDescription = ({book}) => {
   return(
-    <div>
-      {book.name}
+    <div style={{padding: 10}}>
+      <Typography gutterBottom variant="h4">
+        {book.name}
+      </Typography>
     </div>
   );
 };
